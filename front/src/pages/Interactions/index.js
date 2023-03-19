ee853419934e1f6cc4783fa22b2f48d8a5dbf855
marianabetancur18 <mariana.betancurf@perficient.com>
2023-03-19 20:05:15 +0100

    front

---
+++ front/src/pages/Interactions/index.js
@@ -0,0 +1,14 @@
+import { Banner } from "../../components/Banner";
+import { Actions } from "../../components/Actions";
+import { Contact } from "../../components/Contact";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Interacions = () => {
+    return (
+        <>
+            <Actions />
+        </>
+    )
+}
+
+export default Interacions;
