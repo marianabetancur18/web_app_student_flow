07653fbac4071b9970ff31d38ff46112c61b155f
daniel.daza1 <daniel.daza@quipux.com>
2023-04-12 20:20:51 -0500

    fix: elimina argumento innecesario

--- backend/procesamiento/procesamiento_total.py
+++ backend/procesamiento/procesamiento_total.py
@@ -1,7 +1,7 @@
 from procesamiento.materias import lista_materias_cursadas
 
 
-def procesar_historia(historia_academica: str, argumentos: dict):
+def procesar_historia(historia_academica: str):
 
     # TODO: crear cada funcion en un archivo diferente en el que se realice la
     #  extracción de cada parte necesaria para el front
