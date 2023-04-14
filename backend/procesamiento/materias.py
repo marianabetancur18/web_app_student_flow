546fbd280237cb8392e652aa7153c3f27b1949e1
daniel.daza1 <daniel.daza@quipux.com>
2023-04-14 14:36:28 -0500

    feat: crea función de materias faltantes

--- backend/procesamiento/materias.py
+++ backend/procesamiento/materias.py
@@ -2,6 +2,8 @@ import re
 import pandas as pd
 from io import StringIO
 
+from utils.abrir_data import obtener_pensum
+
 
 def lista_materias_cursadas(
         texto_historia_academica: str,
@@ -65,3 +67,41 @@ def lista_materias_cursadas(
     lista_materias_cursadas = materias_cursadas.to_dict("records")
 
     return lista_materias_cursadas
+
+
+def lista_materias_faltantes(
+        texto_historia_academica: str,
+        periodo: list = None,
+        tipo: list = None) -> list:
+
+    """
+    Obtiene una lista de las materias faltantes a partir del texto obtenido del
+     ctrl+a en el sia.
+
+    Args:
+        texto_historia_academica: texto que obtiene toda la historia obtenida con
+         ctrl+a en el sia
+        periodo: periodo académico en caso de que se desee filtrar
+        tipo: tipo de materia en caso de que se quiera fitlrar
+
+    Returns:
+        .. code-block:: python
+
+            [
+                {
+                    'ASIGNATURA': 'Desarrollo Web',
+                    'CREDITOS': '3',
+                    'TIPO': 'Disciplinar obligatoria'
+                },
+                {
+                    'ASIGNATURA': 'Ingeniería de requisitos',
+                    'CREDITOS': '3',
+                    'TIPO': 'Disciplinar obligatoria'
+                },
+            ]
+
+    """
+
+    pensum = obtener_pensum()
+
+    pass
--- backend/procesamiento/procesamiento_total.py
+++ backend/procesamiento/procesamiento_total.py
@@ -1,4 +1,5 @@
 from procesamiento.materias import lista_materias_cursadas
+from procesamiento.materias import lista_materias_faltantes
 
 
 def procesar_historia(historia_academica: str):
@@ -6,7 +7,8 @@ def procesar_historia(historia_academica: str):
     # TODO: crear cada funcion en un archivo diferente en el que se realice la
     #  extracción de cada parte necesaria para el front
     respuesta = {
-        'lista_materias_cursadas': lista_materias_cursadas(historia_academica)
+        'lista_materias_cursadas': lista_materias_cursadas(historia_academica),
+        'materias_faltantes': lista_materias_faltantes(historia_academica)
     }
 
     return respuesta
