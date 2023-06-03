2474542e1f1d6d9a4978317c367a1e46fe4df866
MateoRincon04 <marinconar@unal.edu.co>
2023-06-03 18:58:57 -0500

    fixed connection between front-back

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,31 @@
 # External libraries
 import uvicorn
 from fastapi import FastAPI, APIRouter, Response, File, UploadFile, HTTPException
-
+from fastapi.middleware.cors import CORSMiddleware
 # Own libraries
 from procesamiento.procesamiento_total import procesar_historia
 
 app = FastAPI()
 
-historia = APIRouter(prefix='/historia')
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+historia = APIRouter()
 
 
-@historia.post('/historia', status_code=200)
+@historia.post('/history', status_code=200)
 async def obtener_historia(
         response: Response,
-        archivo_historia: UploadFile = File(...)):
+        file: UploadFile):
     try:
-        texto = archivo_historia.file.read().decode()
-
+        texto = file.file.read().decode()
         # TODO: completar las funciones de procesar historia
         data = procesar_historia(texto)
     except Exception as e:
--- front/src/components/Contact.js
+++ front/src/components/Contact.js
@@ -4,6 +4,7 @@ import contactImg from "../assets/img/contact-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios'
 
 
 export const Contact = () => {
@@ -18,16 +19,20 @@ export const Contact = () => {
     reader.readAsDataURL(file);
     reader.onload = () => resolve(reader.result);
     reader.onerror = error => reject(error);
-});
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     sessionStorage.setItem('file', selectedFile)
     //const body_data = await toBase64(selectedFile)
     console.log(selectedFile)
-    let response = await fetch("http://localhost:80/historia/historia", {
-      method: "POST",
-      body: selectedFile,
+    const formData = new FormData()
+    formData.append('file', selectedFile)
+    console.log(Object.entries(formData))
+    let response = await axios.post("http://localhost:8000/history", formData, {
+      headers: {
+        'Content-Type': "multipart/form-data",
+      }
     });
     sessionStorage.setItem('graph_data', response)
     setButtonText("Sending...");
@@ -42,7 +47,7 @@ export const Contact = () => {
           <Col size={12} md={6}>
             <TrackVisibility>
               {({ isVisible }) =>
-                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
+                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us" />
               }
             </TrackVisibility>
           </Col>
@@ -50,24 +55,24 @@ export const Contact = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h2>Sube tu historia academica para acceder a todas las interacciones!</h2>
-                <form onSubmit={handleSubmit}>
-                  <Row>
-                    <Col size={12} sm={13} className="px-1">
-                      <input type="file" onChange={(e) => setSelectedFile(e.target.files[0])}/>
-                    </Col>
-                    <Col size={12} className="px-1">
-                      <button type="submit"><span>{buttonText}</span></button>
-                    </Col>
-                    {
-                      status.message &&
-                      <Col>
-                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                  <h2>Sube tu historia academica para acceder a todas las interacciones!</h2>
+                  <form onSubmit={handleSubmit}>
+                    <Row>
+                      <Col size={12} sm={13} className="px-1">
+                        <input type="file" onChange={(e) => setSelectedFile(e.target.files[0])} />
                       </Col>
-                    }
-                  </Row>
-                </form>
-              </div>}
+                      <Col size={12} className="px-1">
+                        <button type="submit"><span>{buttonText}</span></button>
+                      </Col>
+                      {
+                        status.message &&
+                        <Col>
+                          <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                        </Col>
+                      }
+                    </Row>
+                  </form>
+                </div>}
             </TrackVisibility>
           </Col>
         </Row>
